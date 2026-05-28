b0c1111705b9327a1a6c84e2ace0134a784235d4
Diego Brito <diegoruivobrito@hotmail.com>
2026-05-27 22:13:48 -0300

    feat: add SWA Terraform resource and GitHub Actions deploy workflow

--- infra/main.tf
+++ infra/main.tf
@@ -16,3 +16,11 @@ resource "azurerm_resource_group" "main" {
   name     = "rg-${var.project_name}"
   location = var.location
 }
+
+resource "azurerm_static_web_app" "main" {
+  name                = "swa-${var.project_name}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku_tier            = "Free"
+  sku_size            = "Free"
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,10 @@
-# Outputs will be added as resources are created
+output "swa_url" {
+  description = "URL pública do Static Web App"
+  value       = azurerm_static_web_app.main.default_host_name
+}
+
+output "swa_deployment_token" {
+  description = "Token usado pelo GitHub Actions para fazer deploy"
+  value       = azurerm_static_web_app.main.api_key
+  sensitive   = true
+}
