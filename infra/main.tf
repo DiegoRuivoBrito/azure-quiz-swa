03b030ab1227c7f461c29287a2b741bfaf68849d
Diego Brito <diegoruivobrito@hotmail.com>
2026-05-28 00:15:16 -0300

    feat: migrate Terraform state to Azure Storage backend

--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stquizswastate"
+    container_name       = "tfstate"
+    key                  = "quiz-swa.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
